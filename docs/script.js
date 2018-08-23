48fb6444623f577acf79ebe11091fb4c8a79baf7
Nelly Duclos <nellyduclos8@gmail.com>
2018-08-23 17:36:53 +0200

    ajout de cartes

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,6 @@
 
 var getCards = Array.from(document.getElementsByClassName("card"));
-
-var createCards = ["😃", "😎","😇","❤️", "💩","😢","😱","😳","😃", "😎","😇","❤️","😢", "😱", "😳"]
+var createCards = ["😃", "😎","😇","❤️","😢","😱","😳","😍", "🤪","🤢","💀","⚽️","👿","😈","🤖","🤡","👽", "😃", "😎","😇","❤️","😢","😱","😳","😍", "🤪","🤢","💀","⚽️","👿","😈","🤖","🤡","👽","💩"]
 
 var compared = ""
 
@@ -50,56 +49,56 @@ getCards.forEach(function(card){
 	}
 })
 
-var getCards = Array.from(document.getElementsByClassName("card"));
+// var getCards = Array.from(document.getElementsByClassName("card"));
 
-		var createCards = ["😃", "😎","😇","❤️","😢","😱","😳","😃", "😎","😇","❤️", "💩","😢", "😱", "😳"]
+// 		var createCards = ["😃", "😎","😇","❤️","😢","😱","😳","😃", "😎","😇","❤️", "💩","😢", "😱", "😳"]
 
 		
 
-		var compared = ""
+// 		var compared = ""
 		
-		getCards.forEach(function(card){
+// 		getCards.forEach(function(card){
 
-			var randomCard = createCards[Math.floor(Math.random() * createCards.length)]
+// 			var randomCard = createCards[Math.floor(Math.random() * createCards.length)]
 
-			var cardIndex = createCards.indexOf(randomCard);
-			var remove = createCards.splice(cardIndex, 1);
+// 			var cardIndex = createCards.indexOf(randomCard);
+// 			var remove = createCards.splice(cardIndex, 1);
 
-			card.innerHTML = randomCard
+// 			card.innerHTML = randomCard
 
-			card.addEventListener('click', reveal)
+// 			card.addEventListener('click', reveal)
 
 
-		 	function reveal(){
-				card.style.color = "red";
-				card.style.backgroundColor = "#fff";
-				var firstCard = compared
-				console.log("first" + firstCard)
-				if(card.innerHTML === "💩"){
-						card.style.color = "red";
-						card.style.backgroundColor = "#fff";
-						setTimeout(function(){
-							var tag = document.getElementById("bigPoo")
-							tag.style.visibility = "visible"
-							var hideCards = document.getElementById("mainCards")
-							hideCards.style.visibility = "hidden"
+// 		 	function reveal(){
+// 				card.style.color = "red";
+// 				card.style.backgroundColor = "#fff";
+// 				var firstCard = compared
+// 				console.log("first" + firstCard)
+// 				if(card.innerHTML === "💩"){
+// 						card.style.color = "red";
+// 						card.style.backgroundColor = "#fff";
+// 						setTimeout(function(){
+// 							var tag = document.getElementById("bigPoo")
+// 							tag.style.visibility = "visible"
+// 							var hideCards = document.getElementById("mainCards")
+// 							hideCards.style.visibility = "hidden"
 							
-						}, 0);
+// 						}, 0);
 
-						setTimeout(function(){
-								location.reload();
-							}, 2000);
-				}
-				else if(compared != card.innerHTML){
-					setTimeout(function()
-						{ card.style.color = "transparent"
-							card.style.backgroundColor = "grey";
-						}, 500
-					)
-				compared = card.innerHTML
-				secondCard = compared
-				console.log("second" + compared)
-				}
+// 						setTimeout(function(){
+// 								location.reload();
+// 							}, 2000);
+// 				}
+// 				else if(compared != card.innerHTML){
+// 					setTimeout(function()
+// 						{ card.style.color = "transparent"
+// 							card.style.backgroundColor = "grey";
+// 						}, 500
+// 					)
+// 				compared = card.innerHTML
+// 				secondCard = compared
+// 				console.log("second" + compared)
+// 				}
 				
-			}
-		})
+// 			}
+// 		})
